3427d0117fb86ef2b8307e4f324e8db65ca48fbb
Hardik Kaushik <24it079@jssaten.ac.in>
2026-08-24 20:30:05 +0530

    Time: 16 ms (19.19%), Space: 48.8 MB (6.56%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -10,12 +10,12 @@ class Solution {
             if(nums[i]!=nums[i-1] && nums[i]!=nums[i-1]+1){
                 miss = nums[i-1]+1;
             }
-        }
-        for(int i = 1; i < nums.length; i++){
             if(nums[i]==nums[i-1]){
                 dup = nums[i];
             }
         }
+            
+        
         if(nums[nums.length-1]!=nums.length){
             miss = nums.length;
         }
